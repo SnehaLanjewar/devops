26aabba9f450473f53f53a81333a00ef0ce50a23
Sneha Lanjewar <Sneha@Apples-MacBook-Pro.local>
2026-06-27 22:08:34 +0530

    Add .gitignore and update Terraform files

--- Terraform/Day-1/ec2-instance-creation/main.tf
+++ Terraform/Day-1/ec2-instance-creation/main.tf
@@ -2,6 +2,8 @@ provider "aws" {
     region = "us-east-1" # set your desired aws region
 }
 
-provider "aws_instance" "example" {
-    
+resource "aws_instance" "example" {
+  ami                     = "ami-0b6d9d3d33ba97d00"
+  instance_type           = "t2.micro"
+   
 }
